dba114108351ad3e106b1599621eb30c85ca0ae5
Viktor Rudnev <me@vrudnev.xyz>
2025-09-22 10:32:15 +0200

    Initial Commit

---
+++ turbokeydion/turbokeydionApp.swift
@@ -0,0 +1,17 @@
+//
+//  turbokeydionApp.swift
+//  turbokeydion
+//
+//  Created by Viktor Rudnev on 22/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct turbokeydionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ turbokeydionTests/turbokeydionTests.swift
@@ -0,0 +1,17 @@
+//
+//  turbokeydionTests.swift
+//  turbokeydionTests
+//
+//  Created by Viktor Rudnev on 22/09/2025.
+//
+
+import Testing
+@testable import turbokeydion
+
+struct turbokeydionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
